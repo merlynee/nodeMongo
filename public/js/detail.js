603aaa1c84f1cbf2f3d291edf735b005916e5afd
merlynee <ping.liu@ihaveu.net>
2016-05-16 18:00:14 +0800

    'alittle'

--- public/js/detail.js
+++ public/js/detail.js
@@ -3,17 +3,30 @@ $(function() {
     var target = $(this)
     var toId = target.data('tid')
     var commentId = target.data('cid')
-    
-    $('<input>').attr({
-      type: 'hidden',
-      name: 'comment[tid]',
-      value: toId
-    }).appendTo('#commentForm')
 
-    $('<input>').attr({
-      type: 'hidden',
-      name: 'comment[cid]',
-      value: commentId
-    }).appendTo('#commentForm')
+
+    if($('#toID').length > 0){
+      $('#toID').val(toId)
+    }
+    else{
+      $('<input>').attr({
+        type: 'hidden',
+        id: 'toID',
+        name: 'comment[tid]',
+        value: toId
+      }).appendTo('#commentForm')
+    }
+
+    if($('#commentId').length > 0){
+      $('#commentId').val(commentId)
+    }
+    else{
+      $('<input>').attr({
+        type: 'hidden',
+        id: 'commentId', 
+        name: 'comment[cid]',
+        value: commentId
+      }).appendTo('#commentForm')
+    }
   })
 })
